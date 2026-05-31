3349d719a3970dd8fe4766491a4c231ee9665fe9
Dhakshatha16 <dhakshathas16@gmail.com>
2026-05-31 20:46:39 +0530

    Update home route message for CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AWS DevOps Pipeline Running Successfully"
+    return "CI/CD Pipeline Working Successfully"
 
 @app.route("/health")
 def health():
